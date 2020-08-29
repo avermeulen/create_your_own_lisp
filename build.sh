bf97f8d262ab557b0e8310d0fb3aeb72c7db1457
avermeulen <andre.vermeulen@gmail.com>
2020-08-29 14:11:47 +0200

    added a grammar using mpc

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ FILE_NAME=$1
 
 echo building $FILE_NAME
 
-cc -std=c99 -Wall -ledit $FILE_NAME.c -o out/$FILE_NAME
+cc -std=c99 -Wall -ledit -lm $FILE_NAME.c mpc.c -o out/$FILE_NAME
 
 echo done building!
--- lispy.c
+++ lispy.c
@@ -4,12 +4,29 @@
 #include <string.h>
 
 #include <editline/readline.h>
-// #include <editline/history.h>
+#include "mpc.h"
 
 // static char input[2048];
 
 
 int main() {
+
+	/* Create Some Parsers */
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Lispy    = mpc_new("lispy");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"                                                     \
+			number   : /-?[0-9]+/ ;                             \
+			operator : '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/ ;   \
+			expr     : <number> | '(' <operator> <expr>+ ')'  ;  \
+			lispy    : /^/ <operator> <expr>+ /$/ | /^/ <expr> <operator> <expr> /$/ ;             \
+		",
+		Number, Operator, Expr, Lispy);
+
+
 	puts("Lipsy version 0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 
@@ -20,13 +37,27 @@ int main() {
 		add_history(input);
 
 		// fgets(input, 2048, stdin);
-		if (strcmp(input, "exit") == 0) {
+		if (strcmp(input, "\\q") == 0) {
 			break;
 		}
 
-		printf("No you're a %s\n", input);
-		free(input);
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			/* On Success Print the AST */
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			/* Otherwise Print the Error */
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+
+
+		// printf("No you're a %s\n", input);
+		// free(input);
 
 	}
+
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
 }
